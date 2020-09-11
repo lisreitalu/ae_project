5fca4fb4e7428829e20d723a00a82f8965dbc845
lisreitalu <54641888+lisreitalu@users.noreply.github.com>
2020-09-11 13:34:31 +0200

    Update models.py

--- posts/models.py
+++ posts/models.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
+    header_image = models.ImageField(null=True, blank=True, upload_to="media/images/", max_length=700)
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -15,9 +16,13 @@ class Post(models.Model):
         return self.title
 
 
+    def save(self):
+        super().save()
+
+
+
     def get_absolute_url(self):
         return reverse('posts-detail', kwargs={'pk': self.pk})
-
 
 
 
